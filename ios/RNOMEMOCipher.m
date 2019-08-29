c47bf6cc544568f319df4b6ae0b4ebc225cf5353
Rimnesh Fernandez <rimnesh.fernandez@gmail.com>
2019-08-29 12:20:40 +0530

    Export module and test method to JS

--- ios/RNOMEMOCipher.m
+++ ios/RNOMEMOCipher.m
@@ -1,3 +1,15 @@
+#import "RNOMEMOCipher.h"
+#import <React/RCTLog.h>
 
-#import <React/RCTBridgeModule.h>
+@implementation RNOMEMOCipher
 
+// To export a module named RNOMEMOCipher
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
+{
+    RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+}
+
+
+@end
